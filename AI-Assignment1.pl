d384b11b061daedbac18b2f75ed292b622d0910f
Abdullah Sadek <102387855+Abdul-Sadek@users.noreply.github.com>
2024-03-19 00:58:36 +0200

    Update AI-Assignment1.pl (Problems 5,6,12)
    problem 12 needs testing

--- AI-Assignment1.pl
+++ AI-Assignment1.pl
@@ -67,3 +67,56 @@ getNumOfItems(CustomerName, OrderID, Length) :-
 	order(CustomerID, OrderID, Items),
 	list_length(Items, Length).	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%fifth problem%%%%%%%%%%%%%%%%%%%
+calcPriceOfOrder(CustomerName, OrderID, TotalPrice) :-
+    customer(CustomerID, CustomerName),
+    order(CustomerID, OrderID, Items),
+    calculateItemsPrice(Items, OrderTotal),
+    TotalPrice is OrderTotal.
+
+calculateItemsPrice([], 0).
+calculateItemsPrice([ItemID|Rest], Total) :-
+    item(ItemID, _, Price),
+    calculateItemsPrice(Rest, RemainingTotal),
+    Total is Price + RemainingTotal.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%sixth problem%%%%%%%%%%%%%%%%%%%
+isBoycott(Item) :-
+    item(Item, Company, _)
+    , boycott_company(Company, _).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%12th problem%%%%%%%%%%%%%%%%%
+% Insert a new item
+add_item(Item, Company, Price) :-
+    assert(item(Item, Company, Price)).
+
+% Remove an item
+remove_item(Item, Company, Price) :-
+    retract(item(Item, Company, Price)).
+
+% Insert a new alternative
+insertAlternative(Item1, Item2) :-
+    assert(alternative(Item1, Item2)).
+
+% Remove an alternative
+removeAlternative(Item1, Item2) :-
+    retract(alternative(Item1, Item2)),
+    retract(alternative(Item2, Item1)). % Remove bidirectional association
+
+% Insert a new boycotted company
+insertBoycott(Company) :-
+    item(Item, Company, Price),
+    assert(boycott_company(Company)).
+
+% Remove a boycotted company
+removeBoycott(Company) :-
+    item(Item, Company, Price),
+    retract(boycott_company(Company)).
+
